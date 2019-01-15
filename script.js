fd7f2c36da6a0ff4a8a25eaf6af1bca74d48dbe9
Anquilis <43358398+Anquilis@users.noreply.github.com>
2019-01-14 16:52:15 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+//Spell #1
+
+$("#spell1").click(function(){
+    $(".frog").slideUp();
+});
+
+
+
+//Spell #2
+$(".appearo").hide();
+
+$("#spell2").click(function(){
+    $(".appearo").toggle();
+});
+
+
+
+//Spell #3
+
+$("#spell3").click(function(){
+    $(".disapear").fadeOut();
+});
+
+
+
